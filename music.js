f0bac2a58726e616dc33c8533f0ec8f0a318a3e0
asimmakhmudov <asimmahmudov18@gmail.com>
2021-05-29 01:21:04 +0400

    musiclub in process

--- music.js
+++ music.js
@@ -2,3 +2,13 @@ const checkbox = document.getElementById("checkbox")
 checkbox.addEventListener('change', () => {
     document.body.classList.toggle('dark');
 });
+var menuList = document.getElementById("menuList");
+        menuList.style.maxHeight = "0px";
+        function toggleMenu(){
+            if (menuList.style.maxHeight == "0px") {
+                menuList.style.maxHeight = "500px";
+            }
+            else {
+                menuList.style.maxHeight = "0px"
+            }
+        }
